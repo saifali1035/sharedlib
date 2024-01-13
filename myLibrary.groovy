921a5f59824beb85b174be61e68f7a28ae5d6ad9
Saif Ali <saifali1035@gmail.com>
2024-01-13 10:33:41 +0530

    Added Lib

---
+++ myLibrary.groovy
@@ -0,0 +1,11 @@
+// src/vars/myLibrary.groovy
+
+def greet(String name) {
+    echo "Hello, ${name}!"
+}
+
+def deployToProduction() {
+    echo "Deploying to production..."
+    // Your deployment logic here
+}
+
